0b6747b101cf8d8e99d07315516ce6a3392516d9
Jon Samwell <jonsamwell@gmail.com>
2014-07-20 20:03:06 +0100

    updated bower file and removed unused dev dependicies

--- dist/jcs-auto-validate.js
+++ dist/jcs-auto-validate.js
@@ -1,5 +1,5 @@
 /*
- * angular-auto-validate - v1.0.7 - 2014-07-20
+ * angular-auto-validate - v1.0.8 - 2014-07-20
  * https://github.com/jonsamwell/angular-auto-validate
  * Copyright (c) 2014 Jon Samwell;*/
 (function (angular) {
