54c0472344748dc2d041876c9a653f03a1f3d444
Jon Samwell <jsamwell@fsatnav.com>
2015-09-21 07:31:23 +1000

    Added pt-pt and zh-tw lang files

--- dist/jcs-auto-validate.js
+++ dist/jcs-auto-validate.js
@@ -1,5 +1,5 @@
 /*
- * angular-auto-validate - v1.18.16 - 2015-09-04
+ * angular-auto-validate - v1.18.16 - 2015-09-21
  * https://github.com/jonsamwell/angular-auto-validate
  * Copyright (c) 2015 Jon Samwell (http://www.jonsamwell.com)
  */
@@ -550,20 +550,6 @@ angular.autoValidate.errorMessages['en-us'] = angular.autoValidate.errorMessages
   pattern: 'Please ensure the entered information adheres to this pattern {0}',
   number: 'Please enter a valid number',
   url: 'Please enter a valid URL in the format of http(s)://www.google.com'
-};
-
-angular.autoValidate.errorMessages['pt-br'] = angular.autoValidate.errorMessages['pt-pt'] = {
-  defaultMsg: 'Por favor adicione uma mensagem de erro para {0}',
-  email: 'Por favor informe um endereço de e-mail válido',
-  minlength: 'Por favor informe no mínimo {0} caracteres',
-  maxlength: 'O tamanho máximo do campo é {0} caracteres.'
-  min: 'Por favor informe um número maior ou igual a {0}',
-  max: 'Por favor informe um número menor que {0}',
-  required: 'Esse campo é obrigatório',
-  date: 'Por favor informe uma data válida',
-  pattern: 'Por favor certifique-se que o dado informado siga o seguinte padrão: {0}',
-  number: 'Por favor informe um número válido',
-  url: 'Por favor informe uma URL válida, no formato http(s)://www.google.com'
 };
 
 function DefaultErrorMessageResolverFn($q, $http) {
