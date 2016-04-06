77e436afe229a0b86ac6a59a379dd4356f0ca310
Conor Hughes <duzawe@gmail.com>
2016-04-06 14:23:26 +0100

    Foundation 6 Form Update SRC
    Modified SRC instead of Dist
    
    Created a foundation6ElementModifier.js for new build

--- dist/jcs-auto-validate.js
+++ dist/jcs-auto-validate.js
@@ -820,6 +820,92 @@ DefaultErrorMessageResolverFn.$inject = [
 
 angular.module('jcs-autoValidate').factory('defaultErrorMessageResolver', DefaultErrorMessageResolverFn);
 
+function Foundation5ElementModifierFn() {
+  var reset = function (el, inputEl) {
+      angular.forEach(el.find('small'), function (smallEl) {
+        if (angular.element(smallEl).hasClass('error')) {
+          angular.element(smallEl).remove();
+        }
+      });
+
+      inputEl.removeClass('error');
+    },
+    findParentColumn = function (el) {
+      var parent = el;
+      for (var i = 0; i <= 3; i += 1) {
+        if (parent !== undefined && (parent.hasClass('columns') || parent.hasClass('column'))) {
+          break;
+        } else if (parent !== undefined) {
+          parent = parent.parent();
+        }
+      }
+
+      return parent;
+    },
+
+    /**
+     * @ngdoc function
+     * @name foundation5ElementModifier#makeValid
+     * @methodOf foundation5ElementModifier
+     *
+     * @description
+     * Makes an element appear valid by apply Foundation 5 specific styles and child elements.
+     * See: http://foundation.zurb.com/docs/components/forms.html
+     *
+     * @param {Element} el - The input control element that is the target of the validation.
+     */
+    makeValid = function (el) {
+      var parentColumn = findParentColumn(el);
+      reset(parentColumn && parentColumn.length > 0 ? parentColumn : el, el);
+    },
+
+    /**
+     * @ngdoc function
+     * @name foundation5ElementModifier#makeInvalid
+     * @methodOf foundation5ElementModifier
+     *
+     * @description
+     * Makes an element appear invalid by apply Foundation 5 specific styles and child elements.
+     * See: http://foundation.zurb.com/docs/components/forms.html
+     *
+     * @param {Element} el - The input control element that is the target of the validation.
+     */
+    makeInvalid = function (el, errorMsg) {
+      var parentColumn = findParentColumn(el),
+        helpTextEl;
+      reset(parentColumn || el, el);
+      el.addClass('error');
+      if (parentColumn) {
+        helpTextEl = angular.element('<small class="error">' + errorMsg + '</small>');
+        parentColumn.append(helpTextEl);
+      }
+    },
+
+    /**
+     * @ngdoc function
+     * @name foundation5ElementModifier#makeDefault
+     * @methodOf foundation5ElementModifier
+     *
+     * @description
+     * Makes an element appear in its default visual state by apply foundation 5 specific styles and child elements.
+     *
+     * @param {Element} el - The input control element that is the target of the validation.
+     */
+    makeDefault = function (el) {
+      makeValid(el);
+    };
+
+  return {
+    makeValid: makeValid,
+    makeInvalid: makeInvalid,
+    makeDefault: makeDefault,
+    key: 'foundation5'
+  };
+}
+
+angular.module('jcs-autoValidate').factory('foundation5ElementModifier', Foundation5ElementModifierFn);
+
+
 function foundation6ElementModifierFn() {
   var reset = function (el, inputEl) {
       angular.forEach(el.find('small'), function (smallEl) {
@@ -1498,9 +1584,10 @@ angular.module('jcs-autoValidate').config(['$provide',
   }
 ]);
 
-function AutoValidateRunFn(validator, defaultErrorMessageResolver, bootstrap3ElementModifier, foundation6ElementModifier) {
+function AutoValidateRunFn(validator, defaultErrorMessageResolver, bootstrap3ElementModifier, foundation5ElementModifier, foundation6ElementModifier) {
   validator.setErrorMessageResolver(defaultErrorMessageResolver.resolve);
   validator.registerDomModifier(bootstrap3ElementModifier.key, bootstrap3ElementModifier);
+  validator.registerDomModifier(foundation5ElementModifier.key, foundation5ElementModifier);
   validator.registerDomModifier(foundation6ElementModifier.key, foundation6ElementModifier);
   validator.setDefaultElementModifier(bootstrap3ElementModifier.key);
 }
@@ -1509,6 +1596,7 @@ AutoValidateRunFn.$inject = [
   'validator',
   'defaultErrorMessageResolver',
   'bootstrap3ElementModifier',
+  'foundation5ElementModifier',
   'foundation6ElementModifier'
 ];
 
--- src/jcs-auto-validate-run.js
+++ src/jcs-auto-validate-run.js
@@ -1,7 +1,8 @@
-function AutoValidateRunFn(validator, defaultErrorMessageResolver, bootstrap3ElementModifier, foundation5ElementModifier) {
+function AutoValidateRunFn(validator, defaultErrorMessageResolver, bootstrap3ElementModifier, foundation5ElementModifier, foundation6ElementModifier) {
   validator.setErrorMessageResolver(defaultErrorMessageResolver.resolve);
   validator.registerDomModifier(bootstrap3ElementModifier.key, bootstrap3ElementModifier);
   validator.registerDomModifier(foundation5ElementModifier.key, foundation5ElementModifier);
+  validator.registerDomModifier(foundation6ElementModifier.key, foundation6ElementModifier);
   validator.setDefaultElementModifier(bootstrap3ElementModifier.key);
 }
 
@@ -9,7 +10,8 @@ AutoValidateRunFn.$inject = [
   'validator',
   'defaultErrorMessageResolver',
   'bootstrap3ElementModifier',
-  'foundation5ElementModifier'
+  'foundation5ElementModifier',
+  'foundation6ElementModifier'
 ];
 
 angular.module('jcs-autoValidate').run(AutoValidateRunFn);
